4471496aaeb467fce9bd3dbe2d45ff45480f8399
Özlem Özdemir <ozllmozdmrr00@gmail.com>
2022-09-14 16:20:13 +0300

    fead: Movie App finish

--- script.js
+++ script.js
@@ -5,36 +5,33 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API =
     'https://api.themoviedb.org/4/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
-
-const form = document.getElementById("form");
-const search = document.getElementById("search");
-const main = document.getElementById("main");
-
-
-getMovies(API_URL)
-
-async function getMovies(url) {
-    const res = await fetch(url);
-    const data = await res.json();
-
-    console.log(data.results);
-    showMovies(data.results);
-}
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
-
-
-    const searchTerm = search.ariaValueMax;
-
-    if (searchTerm && searchTerm !== "") {
-        getMovies(SEARCH_API + searchTerm)
-
-        search.value = ""
-    } else {
-        window.location.reload();
+    const form = document.getElementById('form')
+    const search = document.getElementById('search')
+    const main = document.getElementById('main')
+    
+    getMovies(API_URL)
+    
+    async function getMovies(url) {
+      const res = await fetch(url)
+      const data = await res.json()
+    
+    //   console.log(data.results)
+      showMovies(data.results)
     }
-});
+    
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+    
+      const searchTerm = search.value
+    
+      if (searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+    
+        search.value = ''
+      } else {
+        window.location.reload()
+      }
+    });
 
 function showMovies(movies) {
     main.innerHTML = ""
@@ -50,17 +47,30 @@ function showMovies(movies) {
             <img src="${IMG_PATH + poster_path}"
             alt="${title}" />
         <div class="movie-info">
-            <h3>=${title}</h3>
-            <span class="green">${vote_average}</span>
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
         </div>
         <div class="overview">
-            <h3>=${title}<small>overview</small></h3>
+            <h3>${title}</h3>
             <p>
                ${overview}
             </p>
         </div>
         `
-    });
+
+        main.appendChild(movieEl)
+    })
 };
 
 
+function getClassByRate (vote) {
+    if(vote >=8){
+        return "green"
+    } else if (vote >=5){
+        return "orange"
+    } else {
+        return "red"
+    }
+        
+    
+};
