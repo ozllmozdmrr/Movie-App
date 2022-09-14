871fef52ad6934c5462b51b5006bf943acd758e1
Özlem Özdemir <ozllmozdmrr00@gmail.com>
2022-09-14 16:47:45 +0300

    fead: Movie App finish

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const API_URL =
-  'https:api.themoviedb.org/4/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+  'https:api.themoviedb.org/4/discover/movie?sort_by=popularity.desc&api_key=08052dcf04a069f21a822187d2326183&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API =
-  'https://api.themoviedb.org/4/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+  'https://api.themoviedb.org/4/search/movie?api_key=08052dcf04a069f21a822187d2326183&query="'
 
 const form = document.getElementById('form')
 const search = document.getElementById('search')
